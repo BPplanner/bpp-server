e87c559d61dc3823a9d8642bf9e511b8015208c6
suy2on <tnwjdrms@naver.com>
2021-07-16 23:04:38 +0900

    modify settings.py

--- concept/models.py
+++ concept/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from multiselectfield import MultiSelectField
 
-# Create your models here.
+class StudioConcept(models.Model):
+    HEAD_COUNT_CHOICE = (
+        ('one',)
+    )
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name="concepts")
+    shop_name = models.CharField()
+    profile = models.ImageField()
+    head_count = models.
--- studio/models.py
+++ studio/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from multiselectfield import MultiSelectField
 
-# Create your models here.
+class Shop(models.Model):
+    name = models.CharField()
+    address = models.TextField(max_length=50)
+    minprice = models.IntegerField()
+    price_desc = models.ImageField()
+    profile = models.ImageField()
+    kakaourl = models.URLField()
+    logo = models.ImageField()
+    shop_type = models.IntegerField()
+
+
+

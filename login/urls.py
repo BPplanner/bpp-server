f7c42b688fc0ee253316a30dda4eec027592beda
suy2on <tnwjdrms@naver.com>
2021-07-17 18:03:22 +0900

    simple jwt 세팅작업

--- login/urls.py
+++ login/urls.py
@@ -6,4 +6,5 @@ app_name = 'login'
 urlpatterns = [
     path('rest-auth/kakao/', KakaoLogin.as_view(), name='kakao_login'),
     path('rest-auth/kakao/refresh', kakao_login_refresh, name='kakao_login_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'), # simple jwt
 ]

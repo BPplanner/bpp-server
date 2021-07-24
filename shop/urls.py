389e1710ff56d866b4d667d2b951b1e9f629b598
HoYoungChun <ho723@naver.com>
2021-07-24 12:31:32 +0900

    특정 studio, beautyshop호출 url, view작성

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('studios/', studio_list),
+    path('studios/<int:pk>', shop_detail),
+    path('beautyshops/<int:pk>', shop_detail),
     path('beautyshops/', beautyshop_list),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import *
@@ -15,4 +16,11 @@ def beautyshop_list(request):
     if request.method == 'GET':
         beautyshops = Shop.objects.filter(shop_type = Shop.BEAUTYSHOP)
         serializer = ShopSerializer(beautyshops, many=True)
+        return Response(serializer.data)
+
+@api_view(['GET'])
+def shop_detail(request,pk):
+    if request.method == 'GET':
+        shop = get_object_or_404(Shop, pk=pk)
+        serializer = ShopSerializer(shop)
         return Response(serializer.data)

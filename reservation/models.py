ba74d40eb325732b9e1bd9993dde78c5f1c50203
HoYoungChun <ho723@naver.com>
2021-07-26 19:22:41 +0900

    reservation state choices추가

--- reservation/models.py
+++ reservation/models.py
@@ -4,7 +4,16 @@ from shop.models import *
 
 
 class Reservation(TimeStampMixin):
-    state = models.IntegerField(null=False, blank=False, default=0) # 0 : 문의중 , 1 : 예약확정, 2 : 예약만료
+    INQUIRY = 0 #0 대신 RESERVATION.INQUIRY로 접근하기 위해
+    CONFIRMED = 1
+    EXPIRATION = 2
+    STATE_CHOICES = (
+        (INQUIRY, 'inquiry'),
+        (CONFIRMED, 'confirmed'),
+        (EXPIRATION, 'expiration')
+    )
+
+    state = models.IntegerField(null=False, blank=False, choices=STATE_CHOICES) # 0 : 문의중 , 1 : 예약확정, 2 : 예약만료
     reserved_date = models.DateTimeField(null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)

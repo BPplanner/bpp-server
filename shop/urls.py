46d3612b71f2f23fe7addb95b04c572f9a5da15b
HoYoungChun <ho723@naver.com>
2021-07-27 14:35:42 +0900

    fix: createsuperuser

--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('studios/', studio_list),
-    path('studios/<int:pk>', shop_detail),
-    path('beautyshops/<int:pk>', shop_detail),
     path('beautyshops/', beautyshop_list),
+    path('<int:pk>/', shop_detail),
 ]

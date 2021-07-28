0320a55e76be1f89f6e9f1c78b36f4ca6b2e0377
HoYoungChun <ho723@naver.com>
2021-07-28 18:14:16 +0900

    컨셉필터링 테스트용

--- concept/serializers.py
+++ concept/serializers.py
@@ -4,9 +4,9 @@ from .models import *
 class StudioConceptSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudioConcept
-        #fields = '__all__'
+        fields = '__all__' #TODO 테스트끝나면 아래에 id랑 profile로만 보이게수정필요
         #exclude = ('created_at', 'updated_at')
-        fields = ('id','profile')
+        #fields = ('id','profile')
 
 class OneStudioConceptSerializer(serializers.ModelSerializer):
     class Meta:

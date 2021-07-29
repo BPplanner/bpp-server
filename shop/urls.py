e6f09a380707e16d5904ddc5411a75fa9b95fa2e
HoYoungChun <ho723@naver.com>
2021-07-29 11:35:36 +0900

    convert fbv to cbv

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('<int:pk>/', shop_detail),
-    path('<str:request_shop_type>/', shop_list),
+    path('<int:pk>/', ShopDetail.as_view()),
+    path('<str:request_shop_type>/', ShopList.as_view()),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
+from rest_framework.decorators import APIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from .serializers import *
@@ -8,16 +8,15 @@ from .models import *
 paginator = PageNumberPagination()
 paginator.page_size = 20 #한 page에 들어갈 수
 
-@api_view(['GET'])
-def shop_list(request, request_shop_type):
-    if request_shop_type == "studios": #studio 전체목록 조회
-        shop_type = Shop.STUDIO
-    elif request_shop_type == "beautyshops": #beautyshop 전체목록 조회
-        shop_type = Shop.BEAUTYSHOP
-    else: #url잘못입력
-        return Response(status=404)
+class ShopList(APIView):    
+    def get(self,request, request_shop_type):
+        if request_shop_type == "studios": #studio 전체목록 조회
+            shop_type = Shop.STUDIO
+        elif request_shop_type == "beautyshops": #beautyshop 전체목록 조회
+            shop_type = Shop.BEAUTYSHOP
+        else: #url잘못입력
+            return Response(status=404)
 
-    if request.method == 'GET':
         address = request.query_params.get('address','') #request parameter의 address가져오기(없다면 빈문자열로 가져오기)
         if address:
             studios = Shop.objects.filter(shop_type = shop_type, address= address).order_by('-like_count') #좋아요수 내림차순으로
@@ -28,9 +27,9 @@ def shop_list(request, request_shop_type):
         new_dict = {"return_data": serializer.data}
         return Response(new_dict)
 
-@api_view(['GET'])
-def shop_detail(request,pk):
-    if request.method == 'GET':
+
+class ShopDetail(APIView):
+    def get(self,request,pk):
         shop = get_object_or_404(Shop, pk=pk)
         if shop.shop_type == Shop.STUDIO:
             serializer = OneStudioSerializer(shop,context={"request": request})

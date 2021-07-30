1a6060418f51c5ccfe029899231d9f8f3e40f056
HoYoungChun <ho723@naver.com>
2021-07-30 16:27:17 +0900

    user가 찜한 studio concept 전체목록 조회 구현

--- concept/views.py
+++ concept/views.py
@@ -11,14 +11,24 @@ from .models import *
 
 class StudioConceptList(APIView,PageNumberPagination):
     def get(self,request):
-        #TODO 컨셉필터링 필요
         request_head_count = request.query_params.getlist('head_count')
         request_gender = request.query_params.getlist('gender')
         request_background = request.query_params.getlist('background')
         request_prop = request.query_params.getlist('prop')
         request_dress = request.query_params.getlist('dress') #request parameter에 있는 리스트
         
-        studio_concepts = StudioConcept.objects.all().order_by('-like_count') #좋아요순으로 studio concept 전체 불러오기
+        like = request.query_params.get('like','false') #like parameter 따로 없으면 false로
+        if like=='true':
+            JWT_authenticator = JWTAuthentication()
+            response = JWT_authenticator.authenticate(request)
+            if response is not None:
+                user_id = response[1].payload['user_id']
+                user = get_object_or_404(User,id=user_id) #access_token에서 user가져오기
+                studio_concepts = user.like_studio_concepts.all() #user가 찜한 studio_concept들
+        else:
+            studio_concepts = StudioConcept.objects.all() #찜관련 없이 모든 studio_concept들
+
+        studio_concepts = studio_concepts.order_by('-like_count') #좋아요순으로 studio concept 전체 불러오기
         filtered_studio_concepts=[]
         for studio_concept in studio_concepts:
             if request_head_count==[] or request_head_count==[''] or (set(studio_concept.head_count) & set(request_head_count)): #전체 혹은 겹치는게있으면
@@ -47,13 +57,13 @@ class StudioConceptLike(APIView):
                     return Response({"detail": "already like exist"},status=400)
 
                 LikeStudioConcept.objects.create(studio_concept=studio_concept,user=user) #찜객체 만들기
-                studio_concept.like_count+=1 #shop의 찜수 증가
+                studio_concept.like_count+=1 #studio_concept의 찜수 증가
                 return Response({"result":"studio_concept like create"},status=200)
 
             elif change_to_like=="false":
                 like_studio_concept = get_object_or_404(LikeStudioConcept,studio_concept=studio_concept,user=user) #찜객체 제거(찜객체 애초에 없으면 404)
                 like_studio_concept.delete()
-                studio_concept.like_count-=1 #shop의 찜수 감소
+                studio_concept.like_count-=1 #studio_concept의 찜수 감소
                 return Response({"result":"studio_concept like delete"},status=200)
 
             else:

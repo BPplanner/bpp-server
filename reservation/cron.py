f63ae576fa24f491f59e623a5631cd3c4415c9d3
suy2on <tnwjdrms@naver.com>
2021-07-31 22:52:43 +0900

    cron 파일 수정

--- reservation/cron.py
+++ reservation/cron.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 def reservation_state_change():
     reservations = Reservation.objects.all()
     for reservation in reservations:
-        if reservation.reserved_date < timezone.now():
+        if reservation.reserved_date == Reservation.CONFIRMED and reservation.reserved_date < timezone.now():
             reservation.state = Reservation.EXPIRATION
             reservation.save()
         

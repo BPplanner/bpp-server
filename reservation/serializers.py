1053b5af6b30a26ed17376dd58e3d7d2e33a13ed
suy2on <tnwjdrms@naver.com>
2021-07-31 12:17:43 +0900

    reservation serializer 수정

--- reservation/serializers.py
+++ reservation/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import *
 
-class PickShopSerializer(serializers.ModelSerializer):
+class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
         fields = '__all__'

fd0a96d19f4596718c1935c9f2b859fca02e09f4
suy2on <tnwjdrms@naver.com>
2021-07-31 13:48:21 +0900

    reservation 추가 및 조회 테스팅완료

--- reservation/views.py
+++ reservation/views.py
@@ -20,7 +20,7 @@ def getUser(request):
 
 class AddReservation(APIView):
     def post(self, request, pk, format=None):
-        shop = Shop.objects.filter(id=pk)
+        shop = Shop.objects.get(id=pk)
         reservation = Reservation(state=Reservation.INQUIRY, reserved_date=None, user=getUser(request), shop= shop)
         reservation.save()
         return Response(status=200)
@@ -31,36 +31,37 @@ class ReservationList(APIView, PageNumberPagination):
         reservations = user.reservation_set.all().order_by('-pk')
 
         self.page_size = 10
-        page_result = self.paginate_queryset(reservations)
+        page_result = self.paginate_queryset(reservations, request, view=self)
         serializer = ReservationSerializer(page_result, many=True)
         return Response(serializer.data)
 
 
 class ReservationDetail(APIView):
-    # 특정모델을 찾아주고 없다면 404에러 발생시켜줄 함수
-    def get_object(self, pk):
-        try:
-            return Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            raise Http404
-    
-    def post(self, request, format=None):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-    def patch(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data, partial= True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    pass
+    # # 특정모델을 찾아주고 없다면 404에러 발생시켜줄 함수
+    # def get_object(self, pk):
+    #     try:
+    #         return Post.objects.get(pk=pk)
+    #     except Post.DoesNotExist:
+    #         raise Http404
+    #
+    # def post(self, request, format=None):
+    #     serializer = PostSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #
+    #
+    # def patch(self, request, pk, format=None):
+    #     post = self.get_object(pk)
+    #     serializer = PostSerializer(post, data=request.data, partial= True)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #
+    # def delete(self, request, pk, format=None):
+    #     post = self.get_object(pk)
+    #     post.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)

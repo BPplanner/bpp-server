537978f88003981d9b75584c1c5aeb4c047a04f3
suy2on <tnwjdrms@naver.com>
2021-08-01 11:06:40 +0900

    reservation 조회 필터링 집어넣기

--- reservation/serializers.py
+++ reservation/serializers.py
@@ -1,24 +1,7 @@
 from rest_framework import serializers
 from .models import *
 
-
-class ReservationShopSerializer(serializers.RelatedField):
-    class Meta:
-        model = Shop
-        fields = ("id", "name", "logo", "shop_type", "kakaourl")
-
-
-
 class ReservationSerializer(serializers.ModelSerializer):
-     shop_date = ["shop.id", "shop.name"]
-     shop = serializers.ReadOnlyField(source=shop_date)
-     user = serializers.ReadOnlyField(source="user.id")
-
-
-
-     class Meta:
+    class Meta:
         model = Reservation
-        fields = ("id","state", "reserved_date", "user", "shop")
-
-
-
+        fields = '__all__'
--- reservation/views.py
+++ reservation/views.py
@@ -4,6 +4,7 @@ from .models import *
 from rest_framework.pagination import PageNumberPagination
 from .serializers import *
 import json
+from django.db.models import Q
 # apiview
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -28,9 +29,17 @@ class AddReservation(APIView):
         return Response(status=201)
 
 class ReservationList(APIView, PageNumberPagination):
+
     def get(self, request, format=None):
         user = getUser(request)
-        reservations = user.reservation_set.all().order_by('-pk')
+        params = request.query_params
+
+        print(params['inquiry'])
+        if params['inquiry'] == 'True':
+            reservations = user.reservation_set.filter(state = Reservation.INQUIRY).order_by('-pk')
+        else:
+            print('hear')
+            reservations = user.reservation_set.exclude(state = Reservation.INQUIRY).order_by('-pk')
 
         self.page_size = 10
         page_result = self.paginate_queryset(reservations, request, view=self)
@@ -39,14 +48,13 @@ class ReservationList(APIView, PageNumberPagination):
 
 
 class ReservationDetail(APIView):
+
     def patch(self, request, pk, format=None):
         reservation = get_object_or_404(Reservation, pk=pk)
         reservation.state = Reservation.CONFIRMED
         reservation.reserved_date = json.loads(request.body.decode('utf-8')).get('reserved_date')
         reservation.save()
         return  Response(status=204)
-
-
 
 
     def delete(self, request, pk, format=None):

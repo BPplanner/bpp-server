4d4aa0e90730bcc5edf92173a13444a9251f77f4
suy2on <tnwjdrms@naver.com>
2021-08-02 19:16:40 +0900

    crontab 파일수정

--- reservation/cron.py
+++ reservation/cron.py
@@ -1,11 +1,15 @@
 from .models import *
-from django.utils import timezone
+import datetime
 
 
 def reservation_state_change():
     reservations = Reservation.objects.all()
+    now = datetime.datetime.now()
+    today = now.strftime('%Y-%m-%d')
     for reservation in reservations:
-        if reservation.reserved_date == Reservation.CONFIRMED and reservation.reserved_date < timezone.now():
+        if reservation.state == Reservation.CONFIRMED and reservation.reserved_date < today
             reservation.state = Reservation.EXPIRATION
             reservation.save()
-        
+
+def hello():
+    print("hello")

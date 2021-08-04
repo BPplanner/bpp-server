0026f8a182ebaeb9c109e6f845d2378ecc94f63f
HoYoungChun <ho723@naver.com>
2021-08-04 15:03:19 +0900

    reservation delete patch user check update

--- reservation/views.py
+++ reservation/views.py
@@ -46,33 +46,29 @@ class ReservationDetail(APIView):
     # TODO 예약날짜 오늘날짜 이후인지 확인하는 데코레이터
     def patch(self, request, pk, format=None):
         user = get_user(request)
-        is_match = user.reservation_set.filter(id=pk)  # 해당 예약이 유저의 예약인지 확인 
 
-        if is_match.exists():  # 유저의 예약이 맞다면
-            input_date = datetime.datetime.strptime(json.loads(request.body.decode('utf-8')).get('reserved_date'),
-                                                    '%Y-%m-%d')
-            if input_date < datetime.datetime.now():  # 예약날짜가 오늘 이전일때
-                return Response({"detail": "reserved_date should be after today"}, status=status.HTTP_400_BAD_REQUEST)
-
-            reservation = get_object_or_404(Reservation, pk=pk)
-            reservation.state = Reservation.CONFIRMED
-            reservation.reserved_date = json.loads(request.body.decode('utf-8')).get('reserved_date')  # 예약날짜 저장
-            reservation.save()
-            return Response({"detail": "reserved_date input success"}, status=status.HTTP_204_NO_CONTENT)
-
-        else:  # 유저의 예약이 아니라면 
+        reservation = get_object_or_404(Reservation, pk=pk)
+        if reservation.user != user: # 해당 예약이 유저의 예약인지 확인
             return Response({'detail': 'user has no authority in this reservation'},
                             status=status.HTTP_401_UNAUTHORIZED)
+
+        input_date = datetime.datetime.strptime(json.loads(request.body.decode('utf-8')).get('reserved_date'),
+                                                '%Y-%m-%d')
+        if input_date < datetime.datetime.now():  # 예약날짜가 오늘 이전일때
+            return Response({"detail": "reserved_date should be after today"}, status=status.HTTP_400_BAD_REQUEST)
+
+        reservation.state = Reservation.CONFIRMED
+        reservation.reserved_date = json.loads(request.body.decode('utf-8')).get('reserved_date')  # 예약날짜 저장
+        reservation.save()
+        return Response({"detail": "reserved_date input success"}, status=status.HTTP_204_NO_CONTENT)
 
     def delete(self, request, pk, format=None):
         user = get_user(request)
-        is_match = user.reservation_set.filter(id=pk)  # 해당 예약이 유저의 예약인지 확인 
 
-        if is_match.exists():  # 유저의 예약이 맞다면
-            reservation = get_object_or_404(Reservation, pk=pk)
-            reservation.delete()
-            return Response({"detail": "reservation delete success"}, status=status.HTTP_204_NO_CONTENT)
+        reservation = get_object_or_404(Reservation, pk=pk)
 
-        else:  # 유저의 예약이 아니라면 
+        if reservation.user != user: # 해당 예약이 유저의 예약인지 확인
             return Response({'detail': 'user has no authority in this reservation'},
                             status=status.HTTP_401_UNAUTHORIZED)
+        reservation.delete()
+        return Response({"detail": "reservation delete success"}, status=status.HTTP_204_NO_CONTENT)

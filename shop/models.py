1f4b18b09cc89f2d042c16ef383c5b7f20d81f77
HoYoungChun <ho723@naver.com>
2021-08-05 17:22:02 +0900

    shop code refactoring

--- shop/models.py
+++ shop/models.py
@@ -4,7 +4,7 @@ from login.models import *
 
 class Shop(TimeStampMixin):
     STUDIO = 0  # 0 대신 SHOP.STUDIO로 쓰기위해
-    BEAUTYSHOP = 1
+    BEAUTYSHOP = 1  # 1 대신 SHOP.BEAUTYSHOP로 쓰기위해
     SHOP_TYPE_CHOICES = (
         (STUDIO, 'studio'),
         (BEAUTYSHOP, 'beautyshop'),
@@ -18,27 +18,28 @@ class Shop(TimeStampMixin):
         ('seocho', '서초구')
     )
 
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=100, choices=ADDRESS_CHOICES)
-    address_detail = models.TextField(max_length=100)
-    minprice = models.IntegerField()
+    name = models.CharField(max_length=100)  # shop 이름
+    address = models.CharField(max_length=100, choices=ADDRESS_CHOICES)  # 주소에서 구(강남,강동,...)
+    address_detail = models.TextField(max_length=100)  # 전체주소
+    minprice = models.IntegerField()  # 최소가격
 
-    price_desc = models.ImageField()
-    profile = models.ImageField()  # 컨셉중에서 대표사진1
-    profile_2 = models.ImageField()  # 컨셉중에서 대표사진2
-    profile_3 = models.ImageField()  # 컨셉중에서 대표사진3
-    map = models.ImageField()  # 지도
-    kakaourl = models.URLField()
-    logo = models.ImageField()
+    price_desc = models.ImageField()  # 가격설명 사진
+    profile = models.ImageField()  # 대표사진1
+    profile_2 = models.ImageField()  # 대표사진2
+    profile_3 = models.ImageField()  # 대표사진3
+    map = models.ImageField()  # 지도(약도)사진
+    kakaourl = models.URLField()  # 카카오url
+    logo = models.ImageField()  # 로고사진
     shop_type = models.IntegerField(choices=SHOP_TYPE_CHOICES)  # 0 : studio, 1 : beautyshop
-    like_users = models.ManyToManyField(User, through='LikeShop', related_name="like_shops", blank=True, null=True)
-    like_count = models.PositiveIntegerField(default=0)
+    like_users = models.ManyToManyField(User, through='LikeShop', related_name="like_shops", blank=True,
+                                        null=True)  # 찜한 user들
+    like_count = models.PositiveIntegerField(default=0)  # 찜수
     affiliates = models.ManyToManyField('self', symmetrical=True, blank=True, null=True)  # 제휴업체 대칭적 관계로
 
     def __str__(self):
-        return str(self.shop_type) + ' ' + self.name
+        return self.name
 
 
-class LikeShop(TimeStampMixin):
+class LikeShop(TimeStampMixin):  # Shop에서 like_users 필드에 through
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
--- shop/serializers.py
+++ shop/serializers.py
@@ -1,7 +1,21 @@
-from django.db.models import fields
 from rest_framework import serializers
 from .models import *
 from concept.serializers import *
+
+
+# shop 전체 목록
+class ShopSerializer(serializers.ModelSerializer):
+    like = serializers.SerializerMethodField('is_like')  # user가 특정 shop에 찜을 했는지
+
+    class Meta:
+        model = Shop
+        fields = ('id', 'name', 'address', 'minprice', 'profile', 'like')
+
+    def is_like(self, obj):  # user가 특정 shop에 찜을 했는지
+        if LikeShop.objects.filter(shop=obj.id, user=self.context['user'].id):
+            return True
+        else:
+            return False
 
 
 # OneShopSerializer에서 협력업체 일부 정보만 표시용
@@ -11,41 +25,24 @@ class AffiliateSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'profile')
 
 
-# shop 전체 목록
-class ShopSerializer(serializers.ModelSerializer):
-    like = serializers.SerializerMethodField('is_like')
-
-    class Meta:
-        model = Shop
-        fields = ('id', 'name', 'address', 'minprice', 'profile', 'like')
-        # exclude = ('created_at', 'updated_at', 'shop_type', 'like_users')
-
-    def is_like(self, obj):
-        if LikeShop.objects.filter(shop=obj.id, user=self.context['user'].id):
-            return True
-        else:
-            return False
-
-
 # shop 세부정보
 class OneShopSerializer(serializers.ModelSerializer):
-    affiliates = AffiliateSerializer(read_only=True, many=True)
-    like = serializers.SerializerMethodField('is_like')
-    profiles = serializers.SerializerMethodField('profile_array')
+    affiliates = AffiliateSerializer(read_only=True, many=True)  # 협력업체들
+    like = serializers.SerializerMethodField('is_like')  # user가 특정 shop에 찜을 했는지
+    profiles = serializers.SerializerMethodField('profile_array')  # 대표사진 3개 묶어서 array로 전달
 
     class Meta:
         model = Shop
         fields = (
-            'id', 'name', 'address_detail', 'minprice', 'logo', 'profiles', 'like', 'map',
-            'kakaourl', 'affiliates')
+            'id', 'name', 'address_detail', 'minprice', 'logo', 'profiles', 'like', 'map', 'kakaourl', 'affiliates')
 
-    def is_like(self, obj):
+    def is_like(self, obj):  # user가 특정 shop에 찜을 했는지
         if LikeShop.objects.filter(shop=obj.id, user=self.context['user'].id):
             return True
         else:
             return False
 
-    def profile_array(self, obj):
+    def profile_array(self, obj):  # 대표사진 3개 묶어서 array로 전달
         return [self.context['request'].build_absolute_uri(obj.profile.url),
                 self.context['request'].build_absolute_uri(obj.profile_2.url),
                 self.context['request'].build_absolute_uri(obj.profile_3.url)]

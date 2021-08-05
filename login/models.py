d523206fbc68509fff77ec71996aa01901593f0b
suy2on <tnwjdrms@naver.com>
2021-08-06 01:49:51 +0900

    custom user model 주석추가

--- login/models.py
+++ login/models.py
@@ -12,7 +12,7 @@ class TimeStampMixin(models.Model):
         abstract = True
 
 
-class UserManager(BaseUserManager):
+class UserManager(BaseUserManager): #헬퍼클래스 #user 와 admin의 공통된 것들만 선언
     use_in_migrations = True
 
     def create_user(self, uid, username, password=None):
@@ -29,8 +29,8 @@ class UserManager(BaseUserManager):
     def create_superuser(self, uid, username, password=None):
         user = self.create_user(uid=uid,username=username,)
         user.set_password(password)
-        user.is_admin = True
-        user.is_superuser = True
+        user.is_admin = True  # only superuser
+        user.is_superuser = True # only superuser
         user.save(using=self._db)
         return user
 
@@ -41,7 +41,7 @@ class User(AbstractBaseUser, PermissionsMixin, TimeStampMixin):
     uid = models.PositiveBigIntegerField(unique=True, null=True, default=0)
     username = models.CharField(max_length=100)
     exp = models.DateTimeField(default=timezone.now)
-    refresh = models.TextField(blank=True, null=True)
+    refresh = models.TextField(blank=True, null=True)   #처음에 user정의시 넣지않음
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
@@ -49,8 +49,8 @@ class User(AbstractBaseUser, PermissionsMixin, TimeStampMixin):
         models.Index(fields=['id'])
     ]
 
-    USERNAME_FIELD = 'uid'
-    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = 'uid'   # superuser 생성시 계정  // jwt발급시에도 username대신 쓰임
+    REQUIRED_FIELDS = ['username']      # superuser 생성시 더 받을 정보     
 
     def __str__(self):
         return str(self.uid) + ' ' + self.username
